8841572cfaf488b0e0bf0273fd05d6680deb40e3
Otávio <otaviooasc@gmail.com>
2024-07-21 15:32:13 -0300

    criando controller e service para o usuario

---
+++ src/main/java/repositories/DespesaFixa.java
@@ -0,0 +1,8 @@
+package repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface DespesaFixa extends JpaRepository<DespesaFixa, UUID> {
+}
---
+++ src/main/java/repositories/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package repositories;
+
+
+import domain.usuario.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+}
