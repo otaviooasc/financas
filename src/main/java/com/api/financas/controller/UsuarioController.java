e5483fb20f50a09d267dad58c0834c0c7c29afe7
Danilson1997 <d97.dias@gmail.com>
2024-07-30 00:07:02 -0300

    criação de endpoints de usuario

--- src/main/java/com/api/financas/controller/UsuarioController.java
+++ src/main/java/com/api/financas/controller/UsuarioController.java
@@ -1,9 +1,56 @@
 package com.api.financas.controller;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.api.financas.domain.usuario.Usuario;
+import com.api.financas.repositories.UsuarioRepository;
 
 @RestController
 @RequestMapping("/api/usuario")
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @GetMapping
+    public List<Usuario> getAllUsuarios() {
+        return usuarioRepository.findAll();
+    }
+
+    @PostMapping
+    public Usuario createUsuario(@RequestBody Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
+
+    @GetMapping("/{id}")
+    public Usuario getUsuarioById(@PathVariable UUID id){
+        return usuarioRepository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/{id}")
+    public Usuario updateUsuario(@PathVariable UUID id, @RequestBody Usuario usuarioDetails){
+        Usuario usuario = usuarioRepository.findById(id).orElse(null);
+        if (usuario != null) {
+            usuario.setNome(usuarioDetails.getNome());
+            return usuarioRepository.save(usuario);
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUsuario(@PathVariable UUID id){
+        usuarioRepository.deleteById(id);
+    }
+
 }
