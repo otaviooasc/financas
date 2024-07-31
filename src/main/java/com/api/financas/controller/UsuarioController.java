591e769df77fefb00b840bf1fe1b2a36b5d0f7fd
Danilson1997 <d97.dias@gmail.com>
2024-07-30 22:28:11 -0300

    alteração nos nomes dos metodos

--- src/main/java/com/api/financas/controller/UsuarioController.java
+++ src/main/java/com/api/financas/controller/UsuarioController.java
@@ -24,22 +24,22 @@ public class UsuarioController {
     private UsuarioRepository usuarioRepository;
 
     @GetMapping
-    public List<Usuario> getAllUsuarios() {
+    public List<Usuario> listarTodosUsuarios() {
         return usuarioRepository.findAll();
     }
 
     @PostMapping
-    public Usuario createUsuario(@RequestBody Usuario usuario){
+    public Usuario criarUsuario(@RequestBody Usuario usuario){
         return usuarioRepository.save(usuario);
     }
 
     @GetMapping("/{id}")
-    public Usuario getUsuarioById(@PathVariable UUID id){
+    public Usuario listarUsuarioPorId(@PathVariable UUID id){
         return usuarioRepository.findById(id).orElse(null);
     }
 
     @PutMapping("/{id}")
-    public Usuario updateUsuario(@PathVariable UUID id, @RequestBody Usuario usuarioDetails){
+    public Usuario alterarUsuario(@PathVariable UUID id, @RequestBody Usuario usuarioDetails){
         Usuario usuario = usuarioRepository.findById(id).orElse(null);
         if (usuario != null) {
             usuario.setNome(usuarioDetails.getNome());
@@ -49,7 +49,7 @@ public class UsuarioController {
     }
 
     @DeleteMapping("/{id}")
-    public void deleteUsuario(@PathVariable UUID id){
+    public void deletarUsuario(@PathVariable UUID id){
         usuarioRepository.deleteById(id);
     }
 
